4f16a51a1009f11cb49b887fe49de960e429a707
MayankK21 <mkraone7@gmail.com>
2018-11-06 16:40:06 +0530

    final error page done

--- src/containers/error/error404.js
+++ src/containers/error/error404.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
+import bgimg from './010e02be2d902662207ed5cc1bccf253.jpg'
+import './error404.css';
 
-let imgUrl = "evgeny-grinko.jpg"
-    let styles = {
-        root: {
-            backgroundImage: 'url(' + imgUrl + ')',
-            backgroundSize: 'cover',
-            overflow: 'hidden',
-        }
-    }
 
 class Error404 extends Component {
     render() {
         return (
-            <div  class="text-center">
+            <div className="text-center">
 	            <br/>
-	            <button type="button" class="btn btn-danger text-center" disabled>Error code: 404</button>
+	            <button type="button" className="btn btn-danger text-center" disabled>Error code: 404</button>
 	            <br/><br/>
-	            <h1>OOPS!</h1>
-	            <h1>The Page Was Not Found</h1>
-	            <h2 color="green">Ain't no music here!</h2>
-	            <p>Sorry, but the page you're looking for is not found, has its name changed or is removed.</p>
-	            <button type="button" class="btn btn-primary text-center">Go to Homepage</button>
+	            <div id="textColor">
+		            <h1>OOPS!</h1>
+		            <h1>The Page Was Not Found</h1>
+		            <h2 color="green">Ain't no music here!</h2>
+		            <p>Sorry, but the page you're looking for is not found, has its name changed or is removed.</p>
+		        </div>
+	            <button type="button" className="btn btn-primary text-center">Go to Homepage</button>
 	        </div>
         )
     }
 }
 
-export default Error404;
+export default Error404
